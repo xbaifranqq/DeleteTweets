165c87bdb06bc5cbaea49dcb503569b38b3377e0
xbaifranqq <63120301+xbaifranqq@users.noreply.github.com>
2026-09-07 10:49:44 -0400

    testing
    just testing stuff idk how this app works really

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-var authorization = "Bearer ***"; // replace by authorization value
+var authorization = "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"; // replace by authorization value
 var ua = navigator.userAgentData.brands.map(brand => `"${brand.brand}";v="${brand.version}"`).join(', ');
-var client_tid = "***"; // replace by X-Client-Transaction-Id value
-var client_uuid = "***"; // replace by X-Client-Uuid value
+var client_tid = "Bm3nM3z2KHGSuKZy+P2H6uh2ZBlz3MQjWB8k//lnHvMslc2IJAkuFPXWX1JNGhb1Gr1mSQAsfW3drxuXiv8m48qQ//GjBQ
+"; // replace by X-Client-Transaction-Id value
+var client_uuid = "2f0m9a61-39aa-8edc-8222-81140d2f4d9b"; // replace by X-Client-Uuid value
 var csrf_token = getCookie("ct0");
 var random_resource = "uYU5M2i12UhDvDTzN6hZPg";
 var random_resource_old_tweets = "H8OOoI-5ZE4NxgRr8lfyWg"
 var language_code = navigator.language.split("-")[0]
 var tweets_to_delete = []
 var user_id = getCookie("twid").substring(4);
-var username = "YourUsernameHere" // replace with your username
+var username = "doran_san" // replace with your username
 var stop_signal = undefined
 var twitter_archive_content = undefined
 var twitter_archive_loading_confirmed = false
